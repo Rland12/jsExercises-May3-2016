0182bc5b3df4fa8b08bdd6948601d1c1baa5c78e
Rland12 <rland7596@withs.org>
2016-05-03 14:52:06 +0000

    jsExercises May-3-2016

--- cs-exercises.js
+++ cs-exercises.js
@@ -18,11 +18,15 @@ function exampleTwo(flag) {
 /*
   Create a function named 'checkBirthday' with one parameter.
   Name the parameter anything you want
-  Return "It's you're birthday!" if the parameter is true, otherwise do nothing
+  Return "It's your birthday!" if the parameter is true, otherwise do nothing
   ie:
     checkBirthday(true) -> "It's you're birthday!"
 */
-
+function checkBirthday(candle) {
+  if (candle) {
+    return "It's your birthday!";
+  }
+}
 
 /*
   Create a function named 'checkHoliday' with one parameter.
@@ -31,6 +35,14 @@ function exampleTwo(flag) {
   ie:
     checkHoliday(true) -> "It's time to go to school!"
 */
+function checkHoliday(present) {
+  if (present) {
+    
+  }
+  else{
+     return "It's time to go to school!";
+  }
+}
 
 
 /*
@@ -41,6 +53,14 @@ function exampleTwo(flag) {
     light(true) -> "Turn light off"
     light(false) -> "Turn light on"
 */
+function light(dark) {
+  if (dark) {
+    return "Turn light off"
+  }
+  else{
+     return "Turn light on";
+  }
+}
 
 
 /*
@@ -53,7 +73,15 @@ function exampleTwo(flag) {
     engine(false, true) -> "Shutdown"
     engine(false, false) -> "Shutdown"
 */
+function engine(start, stop) {
+  if (start&&stop) {
+    return "Rev up";
+  }
 
+  else{
+    return "Shutdown";
+  }
+}
 
 /*
   Create a function named 'gradeScore' with one parameter.
@@ -65,7 +93,17 @@ function exampleTwo(flag) {
     gradeScore(81) -> "B"
     gradeScore(80) -> "C"
 */
-
+function gradeScore(good) {
+  if (good > 90) {
+    return "A";
+  }
+  if(good > 80){
+    return "B";
+  }
+  else{
+     return "C";
+  }
+}
 
 /*
   Create a function named 'imDone' with three parameters.
@@ -81,3 +119,17 @@ function exampleTwo(flag) {
     imDone(false, false, true) -> "no"
     imDone(false, false, false) -> "no"
 */
+function imDone(cool,yay,end) {
+  if (cool&&yay) {
+    return "yes";
+  }
+  if(cool&&end){
+    return"yes";
+  }
+  if(yay&&end){
+    return"yes";
+  }
+  else{
+     return "no";
+  }
+}
